aec92d53a2939567981f1e39bc15c8e1450dd23b
EC2 Default User <ec2-user@ip-172-31-8-195.us-east-2.compute.internal>
2018-10-05 16:52:30 +0000

    cucumber

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -2,14 +2,4 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
-  
-  def movies_with_same_director
-    result = []
-    Movie.where(director: self.director).find_each do |movie|
-      if movie != self
-        result += [movie.title]
-      end
-    end
-    return result
-  end
 end
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,59 +1,38 @@
-# Add a declarative step here for populating the DB with movies.
-
 Given /the following movies exist/ do |movies_table|
-  movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
-    Movie.create(movie)
-  end
+    movies_table.hashes.each do |movie|
+        Movie.create!(movie)
+    end
 end
 
-# Make sure that one string (regexp) occurs before or after another one
-#   on the same page
-
-Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  expect(page.body.index(e1)).to be < page.body.index(e2)
+Then /^the director of "(.*)" should be "(.*)"$/ do |title_value, director_value|
+    movie = Movie.find_by_title(title_value)
+    expect(movie.director).to eq director_value
 end
 
-# Make it easier to express checking or unchecking several boxes at once
-#  "When I uncheck the following ratings: PG, G, R"
-#  "When I check the following ratings: G"
+Then /I should see "(.*)" before "(.*)"/ do |movie_title1, movie_title2|
+    body = page.body
+    location_of_movie_title1_in_body= body.index(movie_title1)
+    location_of_movie_title2_in_body= body.index(movie_title2)
+    loc1 = location_of_movie_title1_in_body
+    loc2 = location_of_movie_title2_in_body
+    if loc1==nil || loc2==nil
+        fail "one of both search parameters not found"
+    else
+        expect(loc1<loc2).to eq true
+    end
+end
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  ratings = rating_list.split(' ')
-  ratings.each do |rating|
-    if uncheck
-      uncheck("ratings_#{rating}")
-    else
-      check("ratings_#{rating}")
+    rating_list.split(".").each do |rating|
+        if uncheck
+            step "I uncheck \" ratings_#{rating.strip}\""
+        else
+            step "I check \" ratings_#{rating.strip}\""
+        end
     end
-  end
 end
-
 
 Then /I should see all the movies/ do
-  # Make sure that all the movies in the app are visible in the table
-  movies = Movie.all
-  movies.each do |movie|
-    page.should have_content(movie.title)
-  end
-end
-
-Then /I should (not )?see movies with ratings: (.*)/ do |not_see, rating_list|
-  ratings = rating_list.split(' ')
-  ratings.each do |rating|
-    movies = Movie.where("rating = '#{rating}'")
-    movies.each do |movie|
-      if not_see
-        page.should have_no_content(movie.title)
-      else
-        page.should have_content(movie.title)
-      end
-    end
-  end
+    rows = page.all("table#movies>tbody tr").count
+    expect(rows).to eq Movie.count
 end
